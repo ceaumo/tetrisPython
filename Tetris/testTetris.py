53d64f0bc74f8ff94ff1533463d31eee8909b166
ceaumo <ceaumo.1@gmail.com>
2020-07-15 12:55:35 -0400

    Frame and logo

---
+++ Tetris/testTetris.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root=Tk()
+root.title("Tetris Test")
+root.resizable(0,0)
+root.iconbitmap(r'Tetris\logo.ico')
+
+
+myFrame=Frame(root, width=500, height=650)
+myFrame.pack()
+
+
+
+root.mainloop()
